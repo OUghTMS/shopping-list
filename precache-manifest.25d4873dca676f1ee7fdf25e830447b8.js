123d97370a40d6f6a3f18d1045fb7ec8c820b92d
OUghTMS <misha.saan@gmail.com>
2019-07-06 17:20:33 +0300

    Updates

---
+++ precache-manifest.25d4873dca676f1ee7fdf25e830447b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "890f1185677a7fa5ac6b594280bbf4dc",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "e38ccc4208c7b8cdb48a",
+    "url": "/shopping-list/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/shopping-list/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e38ccc4208c7b8cdb48a",
+    "url": "/shopping-list/static/js/main.f67b11f1.chunk.js"
+  },
+  {
+    "revision": "f83e989e4d16bb609454",
+    "url": "/shopping-list/static/js/runtime~main.e244f075.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shopping-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
